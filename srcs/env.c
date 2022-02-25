1d45bce2a1c0d1ca5eac1532cc3745391dae1b4d
Matteo Bucci <mbucci@fu-r5-p5.s19.be>
2022-02-25 19:06:43 +0100

    env corrected

--- srcs/env.c
+++ srcs/env.c
@@ -6,7 +6,7 @@
 /*   By: mbucci <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 16:22:49 by mbucci            #+#    #+#             */
-/*   Updated: 2022/02/23 16:18:00 by mbucci           ###   ########.fr       */
+/*   Updated: 2022/02/25 19:06:23 by mbucci           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,6 +31,7 @@ void	env(void)
 
 	i = -1;
 	while (datas_prompt.envp[++i])
-		ft_putendl_fd(datas_prompt.envp[i], 1);
+		if (ft_strchr(datas_prompt.envp[i], '='))
+			ft_putendl_fd(datas_prompt.envp[i], 1);
 	datas_prompt.last_command_status = 0;
 }
